feadb41a976ef93c971f35412d176fad4cfe0cbb
Roelof Kallenkoot <roelof000@gmail.com>
2015-09-19 17:13:02 +0200

    batch insert ( niet echt snel )

--- src/ru/vladimir/server/QueueHandler.java
+++ src/ru/vladimir/server/QueueHandler.java
@@ -3,11 +3,24 @@ package ru.vladimir.server;
 import ru.vladimir.model.Measurement;
 import ru.vladimir.worker.MessageQueue;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  * Created by roelof on 19/09/2015.
  */
 public class QueueHandler implements Runnable {
 
+    private final static String url = "jdbc:mysql://192.168.0.39:3306/unwdmi";
+    private final static String user = "unwdmi";
+    private final static String pass = "dUVFLAj97dFeMnZM";
+    private static final String SQL = "INSERT INTO measurements " +
+            "(stn, date, time, temp, dewp, stp, slp, visib, wdsp, prcp, sndp, frshtt, cldc, wnddir) " +
+            "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+    private Connection connection;
     private MessageQueue<Measurement> queue;
 
     /**
@@ -17,21 +30,60 @@ public class QueueHandler implements Runnable {
      */
     public QueueHandler(MessageQueue<Measurement> queue) {
         this.queue = queue;
+
+        try {
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run() {
         // Wat moet de queue handler doen??
         while (true) {
-            if (queue.size() > 8000) {
-                // Onmogelijk dat die null is????
+            if (queue.size() >= 8000) {
+
+                PreparedStatement stmt = null;
+                try {
+                    stmt = connection.prepareStatement(SQL);
+                    connection.setAutoCommit(false);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
                 int size = queue.size();
                 for (int i = 0; i < size; i++) {
                     Measurement m = queue.receive();
-
+                    try {
+                        stmt.setInt(1, m.getStn());
+                        stmt.setDate(2, m.getDate());
+                        stmt.setTime(3, m.getTime());
+                        stmt.setFloat(4, m.getTemp());
+                        stmt.setFloat(5, m.getDewp());
+                        stmt.setFloat(6, m.getStp());
+                        stmt.setFloat(7, m.getSlp());
+                        stmt.setFloat(8, m.getVisib());
+                        stmt.setFloat(9, m.getWdsp());
+                        stmt.setFloat(10, m.getPrcp());
+                        stmt.setFloat(11, m.getSndp());
+                        stmt.setByte(12, m.getFrshtt());
+                        stmt.setFloat(13, m.getCldc());
+                        stmt.setShort(14, m.getWnddir());
+                        stmt.addBatch();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    // Save return values in int[] count en commit
+                    int[] count = stmt.executeBatch();
+                    connection.commit();
+                } catch (SQLException e) {
+                    e.printStackTrace();
                 }
             }
         }
+
     }
 
 }
--- src/ru/vladimir/worker/Worker.java
+++ src/ru/vladimir/worker/Worker.java
@@ -42,15 +42,14 @@ public class Worker implements Runnable {
             while ((inline = input.readLine()) != null) {
                 sb.append(inline.trim());
                 if (inline.equals("</WEATHERDATA>")) {
-                    Document doc = (Document) builder.build(new ByteArrayInputStream(sb.toString().getBytes()));
+                    Document doc = builder.build(new ByteArrayInputStream(sb.toString().getBytes()));
                     List<Element> elements = doc.getRootElement().getChildren("MEASUREMENT");
                     // Validate each measurement
                     // TODO: Hier uitzoeken of fucking java wel echt pass by Reference is kek
                     // Als dat wel zo is hoeven we elements niet te reassignen maar kunnen we
                     // de values in de Validator aanpassen.
                     for (Element e : elements) {
-                        Measurement m = new Measurement(e);
-                        queue.send(m);
+                        queue.send(new Measurement(e));
                     }
                     // Validaten de QueueHandler laten doen ??
 //                    Validator.validateWeatherData(elements);
@@ -61,7 +60,7 @@ public class Worker implements Runnable {
             }
 
             input.close();
-            socket.close();
+            input = null;
         } catch (NullPointerException | IOException ioEx) {
             System.out.println(ioEx.getMessage());
         } catch (JDOMException e) {
@@ -69,6 +68,7 @@ public class Worker implements Runnable {
         } finally {
             try {
                 socket.close();
+                socket = null;
             } catch (IOException e) {
                 e.printStackTrace();
             }
